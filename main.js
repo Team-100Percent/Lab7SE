dce19a9e286c14f138c851beb7748119713e467e
Ilya PC <mr.rusek99@gmail.com>
2020-10-12 22:40:32 +0600

    cleaned code

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-var pi  = 'п';
-var i =0;
+var PI  = 'п';
+var I =0;
+
 function getvalue(num) {
     let a = document.forms[0]["textview"];
+
     a.value += num;
 }
+
 function get_symbyl(){
     let a = document.forms[0]["textview"];
+
     a.value='п';
 }
+
 function getopcode(opt) {
     let a = document.forms[0]["textview"];
     if (a.value != "") {
+
         if (opt == "s") {
             a.value += "**2";
         }else if(opt=='1/x'){
             a.value = '1/'+a.value;
         }else if(opt=='fac'){
             a.value+= 'fac(';
-            i+=1;
+            
+            I+=1;
         }
         else {
             a.value += opt;
@@ -29,27 +36,35 @@ function getopcode(opt) {
     }
     else if (opt == '.') {
         a.value += "0.";
-    }else if(opt=='ss'){
+    }
+    else if(opt=='ss'){
         a.value+= 'Math.sqrt(';
-        i=1;
+
+        I=1;
     }
     else if(opt=='fac'){
         a.value+= 'fac(';
-        i+=1;
-    }
 
+        I+=1;
+    }
 }
+
 function zero() {
     let a = document.forms[0]["textview"];
+    
     if (a.value != "") {
         a.value += "0";
     }
 }
+
 function equal() {
+
     let a = document.forms[0]["result"];
     let b =document.forms[0]["textview"];
-    let c = b.value.replace(pi,'3.14');
+    let c = b.value.replace(PI,'3.14');
+
     a.value = c;
+
     if(i=>1){
         b.value+=')';
     }
@@ -61,42 +76,57 @@ function equal() {
         a.value = "Введите выражение";
     }
     else {
-        a.value = temp;
         let table = document.getElementsByTagName("table")[0];
+
+        a.value = temp;
+        
         if (table.rows.length >= 3) {
             table.deleteRow(0);
         }
+
         let trow = table.insertRow(table.rows.length);
         let cel0 = trow.insertCell(0);
         let cel1 = trow.insertCell(1);
         let cel2 = trow.insertCell(2);
+
         cel0.innerHTML = document.forms[0]["textview"].value;
         cel1.innerHTML = "=";
         cel2.innerHTML = temp;
+
         clean();
     }
 }
+
 function clean() {
     let a = document.forms[0]["textview"];
     a.value = '';
+
     let b = document.forms[0]["result"];
     b.value = '';
 }
+
 function back() {
     let a = document.forms[0]["textview"];
-    a.value = document.forms[0]["textview"].value.substring(0, document.forms[0]["textview"].value.length - 1);
+    a.value = document.forms[0]["textview"]
+        .value.substring(0, document.forms[0]["textview"].value.length - 1);
+
     let b = document.forms[0]["result"];
     b.value = '';
 }
+
 function sinus(x) {
     let a = document.forms[0]["textview"];
+    
     a.value = Math.sin(x);
 }
+
 function fac(x){
     let res = 1;
+
     while(x>1){
         res*=x;
         x-=1;
     }
+
     return res;
 }
